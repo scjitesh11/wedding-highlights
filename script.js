592ab6525880e190e3987777b0d02cdeeff13d69
Satish Choudhury <93796032+scjitesh11@users.noreply.github.com>
2026-07-11 23:08:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Display data from config.js
 
+document.getElementById("title").innerHTML = CONFIG.title;
+
+document.getElementById("coupleName").innerHTML =
+CONFIG.groom + " ❤️ " + CONFIG.bride;
+
+document.getElementById("date").innerHTML =
+"📅 " + CONFIG.weddingDate;
+
+
+// Watch Button
+
+document.getElementById("watchBtn").onclick = function(){
+
+    alert("Our Story Coming Soon ❤️");
+
+};
