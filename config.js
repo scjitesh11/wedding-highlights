df1089631719c6edcd6eb9106f69739cec9bd46f
Satish Choudhury <93796032+scjitesh11@users.noreply.github.com>
2026-07-11 22:58:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,16 @@
 
+const CONFIG = {
+
+    groom: "Satish",
+
+    bride: "Lipsa",
+
+    weddingDate: "24 June 2026",
+
+    venue: "Uttara Nivas, Janiguda",
+
+    song: "music/song.mp3",
+
+    coverImage: "image/cover.jpg"
+
+};
